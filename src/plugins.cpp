7dad2c80480bdd58629eedb0814394656c157e97
Michael Ripperger <michael.ripperger@swri.org>
2024-09-23 16:37:23 -0500

    Added plugins file

---
+++ src/plugins.cpp
@@ -0,0 +1,26 @@
+#include <noether_gui/plugin_interface.h>
+#include <yaml-cpp/yaml.h>
+
+#include <noether_roscon_2024/exercise_2a/mesh_modifier_widget.h>
+
+namespace noether
+{
+struct CylinderSegmentationMeshModifierWidgetPlugin : public MeshModifierWidgetPlugin
+{
+  QWidget* create(QWidget* parent = nullptr, const YAML::Node& config = {}) const override
+  {
+    // Create the widget for the cylinder segmentation mesh modifier
+    auto* widget = new CylinderSegmentationMeshModifierWidget(parent);
+
+    // Attempt to configure the widget
+    if (!config.IsNull())
+      widget->configure(config);
+
+    return widget;
+  }
+};
+
+} // namespace noether
+
+// Export the plugin
+EXPORT_MESH_MODIFIER_WIDGET_PLUGIN(noether::CylinderSegmentationMeshModifierWidgetPlugin, CylinderSegmentation)
