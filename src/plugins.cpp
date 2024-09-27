f8d556a796fdc37d1615176e3318d7b921b019a4
Michael Ripperger <michael.ripperger@swri.org>
2024-09-27 09:25:35 -0500

    Added camera standoff tool path modifier

--- src/plugins.cpp
+++ src/plugins.cpp
@@ -1,6 +1,9 @@
 #include <yaml-cpp/yaml.h>
 #include <noether_gui/plugin_interface.h>
+
+// Includes for the widgets to be used for the plugin
 #include <noether_roscon_2024/exercise_2a/cylinder_segmentation_mesh_modifier_widget.h>
+#include <noether_roscon_2024/exercise_2c/camera_standoff_tool_path_modifier_widget.h>
 
 namespace noether
 {
@@ -19,7 +22,23 @@ struct CylinderSegmentationMeshModifierWidgetPlugin : public MeshModifierWidgetP
   }
 };
 
+struct CameraStandoffToolPathModifierWidgetPlugin : public ToolPathModifierWidgetPlugin
+{
+  QWidget* create(QWidget* parent = nullptr, const YAML::Node& config = {}) const override
+  {
+    // Create the widget for the camera standoff tool path modifier
+    auto* widget = new CameraStandoffToolPathModifierWidget(parent);
+
+    // Attempt to configure the widget
+    if (!config.IsNull())
+      widget->configure(config);
+
+    return widget;
+  }
+};
+
 } // namespace noether
 
 // Export the plugin
 EXPORT_MESH_MODIFIER_WIDGET_PLUGIN(noether::CylinderSegmentationMeshModifierWidgetPlugin, CylinderSegmentation)
+EXPORT_TOOL_PATH_MODIFIER_WIDGET_PLUGIN(noether::CameraStandoffToolPathModifierWidgetPlugin, CameraStandoff)
