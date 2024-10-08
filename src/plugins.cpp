69000030be9947a88bb8d75490540dead3758977
Michael Ripperger <michael.ripperger@swri.org>
2024-10-08 16:49:14 -0500

    Added no-op tool path planner

--- src/plugins.cpp
+++ src/plugins.cpp
@@ -3,6 +3,7 @@
 
 // Includes for the widgets to be used for the plugin
 #include <noether_roscon_2024/exercise_2a/cylinder_segmentation_mesh_modifier_widget.h>
+#include <noether_roscon_2024/exercise_2b/no_op_tool_path_planner_widget.h>
 #include <noether_roscon_2024/exercise_2c/camera_standoff_tool_path_modifier_widget.h>
 
 namespace noether
@@ -13,6 +14,20 @@ struct CylinderSegmentationMeshModifierWidgetPlugin : public MeshModifierWidgetP
   {
     // Create the widget for the cylinder segmentation mesh modifier
     auto* widget = new CylinderSegmentationMeshModifierWidget(parent);
+
+    // Attempt to configure the widget
+    if (!config.IsNull())
+      widget->configure(config);
+
+    return widget;
+  }
+};
+
+struct NoOpToolPathPlannerWidgetPlugin : public ToolPathPlannerWidgetPlugin
+{
+  QWidget* create(QWidget* parent = nullptr, const YAML::Node& config = {}) const override
+  {
+    auto* widget = new NoOpToolPathPlannerWidget(parent);
 
     // Attempt to configure the widget
     if (!config.IsNull())
@@ -41,4 +56,5 @@ struct CameraStandoffToolPathModifierWidgetPlugin : public ToolPathModifierWidge
 
 // Export the plugin
 EXPORT_MESH_MODIFIER_WIDGET_PLUGIN(noether::CylinderSegmentationMeshModifierWidgetPlugin, CylinderSegmentation)
+EXPORT_TPP_WIDGET_PLUGIN(noether::NoOpToolPathPlannerWidgetPlugin, NoOpPlanner)
 EXPORT_TOOL_PATH_MODIFIER_WIDGET_PLUGIN(noether::CameraStandoffToolPathModifierWidgetPlugin, CameraStandoff)
