b13e34be55fe8ada2ef1f8db497586126aca0991
Michael Ripperger <michael.ripperger@swri.org>
2024-10-08 14:28:40 -0500

    Added projection onto cylinder

--- src/exercise_2a/cylinder_segmentation_mesh_modifier.cpp
+++ src/exercise_2a/cylinder_segmentation_mesh_modifier.cpp
@@ -1,18 +1,48 @@
 #include <noether_roscon_2024/exercise_2a/cylinder_segmentation_mesh_modifier.h>
 
+// Include for noether utilties
+#include <noether_tpp/mesh_modifiers/subset_extraction/subset_extractor.h>
+#include <noether_tpp/utils.h>
+
 // Include SAC segmentation from PCL
 #include <pcl/sample_consensus/ransac.h>
 #include <pcl/sample_consensus/sac_model_cylinder.h>
 #include <pcl/conversions.h>
 
-// Include function from `noether` for extracting a sub-mesh from vertex inliers
-#include <noether_tpp/mesh_modifiers/subset_extraction/subset_extractor.h>
+// Include for pcl::concatenateFields
+#include <pcl/common/io.h>
 
 // Include for std::iota
 #include <numeric>
 
 namespace noether
 {
+pcl::PolygonMesh project(const pcl::PolygonMesh& mesh,
+                         pcl::Indices& inliers,
+                         pcl::shared_ptr<pcl::SampleConsensusModelCylinder<pcl::PointXYZ, pcl::Normal>> model,
+                         pcl::shared_ptr<pcl::RandomSampleConsensus<pcl::PointXYZ>> ransac)
+{
+  Eigen::VectorXf coefficients;
+  ransac->getModelCoefficients(coefficients);
+  pcl::PointCloud<pcl::PointXYZ> projected_inliers;
+  model->projectPoints(inliers, coefficients, projected_inliers, false);
+
+  // TODO: Update point normals based on model coefficients
+
+  // Convert from point cloud object to message type
+  pcl::PCLPointCloud2 projected_inliers_msg;
+  pcl::toPCLPointCloud2(projected_inliers, projected_inliers_msg);
+
+  // Create the output mesh
+  // Use `pcl::concatenateFields` to overwrite the xyz fields of the original mesh vertices with the xyz fields of the projected vertices
+  // while retaining the information in all other fields (e.g., color, etc)
+  pcl::PolygonMesh projected_mesh;
+  projected_mesh.polygons = mesh.polygons;
+  pcl::concatenateFields(mesh.cloud, projected_inliers_msg, projected_mesh.cloud);
+
+  return projected_mesh;
+}
+
 CylinderSegmentationMeshModifier::CylinderSegmentationMeshModifier(float min_radius,
                                                                    float max_radius,
                                                                    float distance_threshold,
@@ -60,8 +90,9 @@ std::vector<pcl::PolygonMesh> CylinderSegmentationMeshModifier::modify(const pcl
 
   // Create a vector of indices for the remaining indices to which a cylinder model can be fit
   // To start, all indices are remaining (i.e., [0, 1, 2, ..., cloud->size() - 1])
-  std::vector<int> remaining_indices(cloud_points->size());
-  std::iota(remaining_indices.begin(), remaining_indices.end(), 0);
+  std::vector<int> all_indices(cloud_points->size());
+  std::iota(all_indices.begin(), all_indices.end(), 0);
+  std::vector<int> remaining_indices = all_indices;
 
   /* Detect as many cylinders as possible while:
    *   - there are at least enough vertices left to form a new model cluster, and
@@ -74,18 +105,25 @@ std::vector<pcl::PolygonMesh> CylinderSegmentationMeshModifier::modify(const pcl
     if (!ransac->computeModel())
       break;
 
+    // Refine the fit model
+    if (!ransac->refineModel())
+      break;
+
     // Extract the inliers and ensure there are enough to form a valid model cluster
     std::vector<int> inliers;
     ransac->getInliers(inliers);
     if (inliers.size() < min_vertices_)
       break;
 
+    // Project the mesh onto the cylinder model
+    // We cannot (easily) project only the inlier vertices after extracting the sub-mesh because the inliers of sub-mesh extraction can be a subset of the original inliers due to the removal of unreferenced vertices.
+    // Therefore, we choose to project all vertices onto the model and then perform sub-mesh extraction for simplicity
+    pcl::PolygonMesh projected_mesh = project(mesh, all_indices, model, ransac);
+
     // Extract the inlier submesh
-    pcl::PolygonMesh output_mesh = extractSubMeshFromInlierVertices(mesh, inliers);
+    pcl::PolygonMesh output_mesh = extractSubMeshFromInlierVertices(projected_mesh, inliers);
     if (!output_mesh.polygons.empty())
     {
-      // TODO: Project the inlier vertices onto the cylinder
-
       // Append the extracted and projected mesh to the vector of output meshes
       output.push_back(output_mesh);
     }
